385c15d1b27cbef38fb308670ca0d04095bea010
Ayesha-Aziz <moizs4644@gmail.com>
2024-11-08 13:36:47 +0500

    update

--- components/Stairs.tsx
+++ components/Stairs.tsx
@@ -21,25 +21,24 @@ const Stairs = () => {
   return (
     <>
       {[...Array(6)].map((_, index) => {
-        return(
-        <motion.div
-          key={index}
-          variants={stairAnimation}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          transition={{
-            duration: 0.4,
-            ease: "easeInOut",
-            delay: reverseIndex(index) * 0.1,
-          }}
-          className="h-full w-full bg-white relative"
-        />
-        )
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
       })}
-    
     </>
-  )
+  );
 };
 
 export default Stairs;

39ae1747a729123e8b1ba48b40d79e756f822b84
Ayesha Shaikh <163914368+ayesha-aziz123@users.noreply.github.com>
2025-02-26 23:17:16 +0500

    Update page.tsx

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -80,7 +80,7 @@ const About = () => {
               width={80}
               height={80}
             />
-            <p className="mt-3 font-bold text-xl text-white text-center">{aboutInfo.projects} </p>
+            <p className="mt-3 font-bold text-xl text-white text-center">{aboutInfo.project} </p>
             <p className="text-base sm:text-lg text-gray-400 text-center">Completed Project</p>
           </div>
           {/* 4 */}
